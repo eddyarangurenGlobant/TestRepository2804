e40066a53c19e23c0d93365515412f21cac4365c
eddyarangurenGlobant <eddy.aranguren@globant.com>
2022-04-28 16:07:10 -0500

    first test develop

---
+++ src/Data/Test.java
@@ -0,0 +1,10 @@
+package Data;
+
+public class Test {
+    public Test(){
+
+    }
+    public void print(){
+        System.out.println("Hello");
+    }
+}
---
+++ src/Runner/App.java
@@ -0,0 +1,11 @@
+package Runner;
+
+import Data.Test;
+
+public class App {
+    public static void main(String[] args) {
+        Test test = new Test();
+        test.print();
+    }
+
+}
