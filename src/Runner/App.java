f3edcfd7d47e0e2cba17e6f632d5b2f8a19bc919
eddyarangurenGlobant <eddy.aranguren@globant.com>
2022-05-08 00:31:36 -0500

    adding utils

--- src/Runner/App.java
+++ src/Runner/App.java
@@ -1,11 +1,13 @@
 package Runner;
 
 import Data.Test;
+import Utils.Helper;
 
 public class App {
     public static void main(String[] args) {
         Test test = new Test();
         test.print();
+        Helper.Printing();
     }
 
 }
---
+++ src/Utils/Helper.java
@@ -0,0 +1,7 @@
+package Utils;
+
+public class Helper {
+    public static void Printing(){
+        System.out.println("Helper is working");
+    }
+}
